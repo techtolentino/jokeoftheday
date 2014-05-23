7c17c6febf1dc768ce5918ed0472008d3e385567
Randy Tolentino <makertolentino@gmail.com>
2014-05-23 11:37:34 -0500

    stuck

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -1,14 +1,15 @@
 require "spec_helper"
 require "sqlite3"
 require "fileutils"
+require "pry-debugger"
 
 describe "db" do
   before(:each) do
     @db = Joke::DB.new("app.db")
   end
-  after(:each) do
-    FileUtils.rm("app.db") if File.exists?("app.db")
-  end
+  # after(:each) do
+  #   FileUtils.rm("app.db") if File.exists?("app.db")
+  # end
 
   it "exists" do
     expect(Joke::DB).to be_a(Class)
@@ -23,6 +24,24 @@ describe "db" do
     db2 = Joke.db
     expect(db1).to be(db2)
   end
+
+  it "can return a hash" do
+    db1 = Joke.db
+    expect(db1.jokes).to be_a(Hash)
+  end
+
+  it "build_a_joke can build a joke from data" do
+    joke1 = @db.build_joke(:id => 1, :joke => "What is life", :answer => "a joke")
+
+    expect(joke1.id).to eq(1)
+    expect(joke1.joke).to eq("What is life")
+    expect(joke1.answer).to eq("a joke")
+  end
+
+  it "create_joke can store a joke in the database" do
+
+  end
+    # binding.pry
 
 
 end
